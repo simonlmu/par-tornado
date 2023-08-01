38e76dd5580d66486e99267a78e1160088122430
Eduard von Briesen <eduard.von-briesen@adesso.de>
2023-08-01 10:51:23 +0200

    added hint counter

--- par_tornado/Assets/PAR_Tornado/ModalController.cs
+++ par_tornado/Assets/PAR_Tornado/ModalController.cs
@@ -1,9 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ModalController : MonoBehaviour
 {
     public GameObject modalPanel;
+    public GameObject button;
+    
+    [SerializeField] 
+    private TMP_Text _hints;
+
+    private int counter;
+
+    private void setModalText(string text)
+    {
+        if(_hints != null)
+        {
+            _hints.text = text;
+        }
+        counter++;
+    }
 
     public void ShowModal()
     {
@@ -11,6 +27,11 @@ public class ModalController : MonoBehaviour
         {
             modalPanel.SetActive(true);
         }
+        if(button != null)
+        {
+            button.SetActive(false);
+        }
+        setModalText("Modal: " + counter);
     }
 
     public void HideModal()
@@ -18,6 +39,10 @@ public class ModalController : MonoBehaviour
         if(modalPanel != null)
         {
             modalPanel.SetActive(false);
+        }
+        if(button != null)
+        {
+            button.SetActive(true);
         }
     }
 
@@ -27,5 +52,10 @@ public class ModalController : MonoBehaviour
         {
             modalPanel.SetActive(!modalPanel.activeSelf);
         }
+        if(button != null)
+        {
+            button.SetActive(!button.activeSelf);
+        }
+        setModalText("Modal: " + counter);
     }
 }
